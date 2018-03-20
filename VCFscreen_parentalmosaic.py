13687b7cddaffb0bdccb8291a98a8db925be9186
Matt Halvorsen <mhalvors1@gmail.com>
2018-03-20 18:04:00 -0400

    debugging thresholding on internal ctrl-only af

--- VCFscreen_parentalmosaic.py
+++ VCFscreen_parentalmosaic.py
@@ -155,8 +155,8 @@ def main(ARGS = None):
 
         ## filter on internal ctrl-only maf, can't do in var cnds file          
         if args.internal_ctrl_af_max != None:                                   
-            ctrl_af = cyvcf_variant.compute_maf(ctrl_idxs)                      
-            if ctrl_af >= args.internal_ctrl_af_max:                            
+            ctrl_af = cyvcf2_variant.compute_maf(ctrl_idxs)                      
+            if ctrl_af > args.internal_ctrl_af_max:                            
                 continue 
 
         ## variant cnds file provided, filter exclusively on that
